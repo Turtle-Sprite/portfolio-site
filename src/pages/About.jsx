b4f93694ad3c5d1ed8de0aae59d5d126780eaccd
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2023-02-20 23:33:40 -0800

    Made some changes. Need to investigate errors on deployment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,30 @@
 import Loader from 'react-loaders';
 import "../index.scss"
-import photo from '../images/Photo.jpg'
+import "../partials/AnimatedLetters.scss"
 import AnimatedAbout from './about/AnimatedAbout';
 import AnimatedCube from './about/AnimatedCube';
 import TextZone from './about/TextZone';
 
 function About() {
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch('SamplePDF.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'SamplePDF.pdf';
+                alink.click();
+            })
+        })
+    }
     
     return (
         <>
             <div className='about-container flex gap-5 flex-wrap'>
+               <button>Hire me</button>
                 <div className='aboutImg m-3 '>
                     <AnimatedAbout />
                 </div>
--- src/pages/about/TextZone.jsx
+++ src/pages/about/TextZone.jsx
@@ -2,7 +2,7 @@
 function TextZone() {
     return ( 
         <>
-        <h1>Web Application Developer</h1>
+        <h1>Web Developer</h1>
         <h2> Javascript / Reactjs / Nodejs</h2>
         <h3>I am a Software Engineer from General Assembly's Bootcamp where I built full-stack web applications using React, Nodejs, MongoDB, and Express. With 7 years experience in project management and teaching, I was able to apply my ability to learn new technologies, design efficient workflows, and implement creative problem-solving in fast-paced environments. I am looking for a role developing web-based applications.</h3>
         </>
