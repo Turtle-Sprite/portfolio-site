e6e87141f66fc3689bd051fe480bb94a5f19a0e6
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2023-02-20 23:51:06 -0800

    Added a resume download button

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,14 +8,14 @@ import TextZone from './about/TextZone';
 function About() {
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('SamplePDF.pdf').then(response => {
+        fetch('C-Sheeley-Resume.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'SamplePDF.pdf';
+                alink.download = 'C-Sheeley-Resume.pdf';
                 alink.click();
             })
         })
@@ -24,10 +24,10 @@ function About() {
     return (
         <>
             <div className='about-container flex gap-5 flex-wrap'>
-               <button>Hire me</button>
                 <div className='aboutImg m-3 '>
                     <AnimatedAbout />
                 </div>
+               <button className='btn hover:bg-yellow-400' onClick={() => onButtonClick()}>Hire me</button>
 
                 <div className='text-zone m-3'>
                     <TextZone />
